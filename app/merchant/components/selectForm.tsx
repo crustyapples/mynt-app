247e840ee8a29e262804358274d5fa30dc144f44
Liam Wells Ayathan <ayathan21@gmail.com>
2023-04-10 11:59:23 +0800

    small change to conductSelect function

--- app/merchant/components/selectForm.tsx
+++ app/merchant/components/selectForm.tsx
@@ -80,7 +80,7 @@ const SelectForm = ({
       };
       console.log("updating losers")
       const message = `Unsuccessful message for ${eventName2}`;
-      const telegramPush = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${losers[i].chat_id}&text=${message}`;
+      const telegramPush = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${(losers[i] as any).chat_id}&text=${message}`;
       fetch(telegramPush).then((res) => {
         console.log(res);
       }).then(() => {
